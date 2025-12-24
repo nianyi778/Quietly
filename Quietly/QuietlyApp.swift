a65382516a9ee3d8bee00f367a98858e3d6f626d
likai <kai.li@elestyle.jp>
2025-12-24 10:39:26 +0900

    Initial Commit

---
+++ Quietly/QuietlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuietlyApp.swift
+//  Quietly
+//
+//  Created by likai on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct QuietlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuietlyTests/QuietlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuietlyTests.swift
+//  QuietlyTests
+//
+//  Created by likai on 2025/12/24.
+//
+
+import Testing
+@testable import Quietly
+
+struct QuietlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
